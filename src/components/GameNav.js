8bcf480fae2dbd272d5ddad0e6881b7807da803d
wassim <wbvb41@gmail.com>
2023-04-04 05:14:00 +0200

    setup responsivity

--- src/components/GameNav.js
+++ src/components/GameNav.js
@@ -1,15 +1,17 @@
 import React from "react";
+import "../index.css";
 
 function GameNav({ undoMove, resetChessBoard, status }) {
   return (
-    <div className="flex flex-col px-12 py-6">
+    <div className="flex flex-col px-6 py-3 sm:px-12 sm:py-6">
       <div className="flex justify-between">
         <button>Main Menu</button>
         <h1>{status}</h1>
-        <button onClick={resetChessBoard}>Restart</button>
+        <h2>Info</h2>
       </div>
-      <div>
+      <div className="flex justify-between">
         <button onClick={undoMove}> &#10550; Undo</button>
+        <button onClick={resetChessBoard}>Restart</button>
       </div>
     </div>
   );
