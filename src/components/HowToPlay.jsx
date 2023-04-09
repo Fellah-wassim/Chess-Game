8aaef93aa81cfcdebf83601852eab1b496beedd4
wassim <wbvb41@gmail.com>
2023-04-09 07:36:02 +0200

    added the back button to how to play documentation

--- src/components/HowToPlay.jsx
+++ src/components/HowToPlay.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-scroll";
+import { Link as LinkRouter } from "react-router-dom";
 import setupChessboardImg from "../assets/howToPlay/setupChessboard.gif";
 
 const HowToPlay = () => {
@@ -40,6 +41,22 @@ const HowToPlay = () => {
       <div className="w-[95%] bg-lightenBrown px-6 py-4 sm:px-12 sm:w-[70%] lg:w-[50%]">
         <ul className="documentation-ul flex flex-col gap-12">
           <li>
+            <div className="self-start">
+              <LinkRouter
+                to="/"
+                className="nav-btn flex items-center justify-center gap-2 bg-secondBlack p-2 px-4 rounded-md text-white hover:bg-lightenBrown hover:text-secondBlack  duration-300 border-2 border-secondBlack"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  id="back"
+                  className="w-[25px]"
+                >
+                  <path d="M19,5H9.83a3,3,0,0,0-2.12.88L2.29,11.29a1,1,0,0,0,0,1.42l5.42,5.41A3,3,0,0,0,9.83,19H19a3,3,0,0,0,3-3V8A3,3,0,0,0,19,5Zm1,11a1,1,0,0,1-1,1H9.83a1.05,1.05,0,0,1-.71-.29L4.41,12,9.12,7.29A1.05,1.05,0,0,1,9.83,7H19a1,1,0,0,1,1,1ZM16.71,9.29a1,1,0,0,0-1.42,0L14,10.59l-1.29-1.3a1,1,0,0,0-1.42,1.42L12.59,12l-1.3,1.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L14,13.41l1.29,1.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L15.41,12l1.3-1.29A1,1,0,0,0,16.71,9.29Z"></path>
+                </svg>
+                <p className="text-xl">Back</p>
+              </LinkRouter>
+            </div>
             <h1 className="text-4xl text-center font-bold text-white bg-secondBlack px-6 py-3 my-6 rounded-md">
               Learn How to Play Chess
             </h1>
