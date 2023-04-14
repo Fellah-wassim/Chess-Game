ec39641eb15bb9010d1200e990e86e84824a87a4
wassim <wbvb41@gmail.com>
2023-04-14 18:49:31 +0200

    added the sound effect to the game

--- src/components/Game/GameNav.jsx
+++ src/components/Game/GameNav.jsx
@@ -7,6 +7,11 @@ import checkIcon from "../../assets/nav/check.svg";
 import moveIcon from "../../assets/nav/move.svg";
 import checkmateIcon from "../../assets/nav/checkmate.svg";
 import drawIcon from "../../assets/nav/draw.svg";
+import moveAudio from "../../assets/audio/move.mp3";
+import gameStartAudio from "../../assets/audio/game-start.mp3";
+import checkAudio from "../../assets/audio/check.mp3";
+import checkmateAudio from "../../assets/audio/checkmate.mp3";
+import stalemateAudio from "../../assets/audio/stalemate.mp3";
 
 function GameNav(props) {
   const restartClicked = () => {
@@ -31,6 +36,18 @@ function GameNav(props) {
     return moveIcon;
   };
 
+  const playMoveAudio = () => {
+    let audio = new Audio(gameStartAudio);
+    if (props.game.current?._history?.length > 0) audio = new Audio(moveAudio);
+    if (props.game.current?.isGameOver()) {
+      if (props.game.current?.isDraw()) audio = new Audio(stalemateAudio);
+      audio = new Audio(checkmateAudio);
+    }
+    if (props.check) audio = new Audio(checkAudio);
+    if (props.gameOverByTimeout) audio = new Audio(checkmateAudio);
+    audio.play();
+  };
+
   return (
     <div className="text-bold text-secondBlack text-xl">
       <div className="flex flex-col bg-secondWhite text-lg sm:text-xl text-white px-2 py-4 sm:px-12 sm:py-6">
@@ -53,7 +70,10 @@ function GameNav(props) {
             className={`${StatusStyle()} w-[60%] sm:w-[35%] flex justify-center items-center text-center gap-2 text-2xl p-1 px-3  rounded-md border-2 border-secondBlack left-1/2 translate-x-[-50%] absolute bottom-5 sm:static sm:translate-x-0 sm:p-2 sm:px-6`}
           >
             <img src={StatusIcon()} alt="checkmate icon" className="w-[35px]" />
-            <p>{props.status.toUpperCase()}</p>
+            <p>
+              {props.status.toUpperCase()}
+              {playMoveAudio()}
+            </p>
           </h1>
 
           <button
